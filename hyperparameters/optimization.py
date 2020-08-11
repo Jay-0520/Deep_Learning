b6edde85f74746b96bde4aa35f85375f8f922bfc
JINGJING HUANG <jingjinghuang@jingjings-mbp.sickkids.ca>
2020-08-11 16:48:34 -0400

    add run function to optimization.py

--- hyperparameters/optimization.py
+++ hyperparameters/optimization.py
@@ -315,4 +315,52 @@ def model(X, Y, layers_dims, optimizer, learning_rate = 0.0007, mini_batch_size 
     return parameters
 
 
+if __name__ == '__main__':
+    
 
+    # ===== Mini-batch Gradient descent ======= 
+    # train 3-layer model
+    layers_dims = [train_X.shape[0], 5, 2, 1]
+    parameters = model(train_X, train_Y, layers_dims, optimizer = "gd")
+    
+    # Predict
+    predictions = predict(train_X, train_Y, parameters)
+    
+    # Plot decision boundary
+    plt.title("Model with Gradient Descent optimization")
+    axes = plt.gca()
+    axes.set_xlim([-1.5,2.5])
+    axes.set_ylim([-1,1.5])
+    plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+    
+    
+    # ===== Mini-batch gradient descent with momentum ======
+    # train 3-layer model
+    layers_dims = [train_X.shape[0], 5, 2, 1]
+    parameters = model(train_X, train_Y, layers_dims, beta = 0.9, optimizer = "momentum")
+    
+    # Predict
+    predictions = predict(train_X, train_Y, parameters)
+    
+    # Plot decision boundary
+    plt.title("Model with Momentum optimization")
+    axes = plt.gca()
+    axes.set_xlim([-1.5,2.5])
+    axes.set_ylim([-1,1.5])
+    plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+    
+    
+    # ===== Mini-batch with Adam mode =====
+    # train 3-layer model
+    layers_dims = [train_X.shape[0], 5, 2, 1]
+    parameters = model(train_X, train_Y, layers_dims, optimizer = "adam")
+    
+    # Predict
+    predictions = predict(train_X, train_Y, parameters)
+    
+    # Plot decision boundary
+    plt.title("Model with Adam optimization")
+    axes = plt.gca()
+    axes.set_xlim([-1.5,2.5])
+    axes.set_ylim([-1,1.5])
+    plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
